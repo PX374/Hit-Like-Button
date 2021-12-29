7263e38c7bfa774c8db96186c2840876a355cd9c
M.Wolf <96348252+PX374@users.noreply.github.com>
2021-12-29 11:57:00 +0100

    Create like_all.js

---
+++ like_all.js
@@ -0,0 +1,4 @@
+b_l = document.getElementsByClassName("oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 pq6dq46d mg4g778l btwxx1t3 pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb lzcic4wl abiwlrkh p8dawk7l");
+for (var i = 0; i < b_l.length ; i++) {
+  if (b_l[i].label = "J'aime") {  b_l[i].click();}
+}
